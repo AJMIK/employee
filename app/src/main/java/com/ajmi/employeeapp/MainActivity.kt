a6c6b74bc2cca8f21709aa68a4544eaa3b3988eb
AJMIK <ajmimkabeer24@gmail.com>
2020-12-12 15:08:44 +0530

    code changed

--- app/src/main/java/com/ajmi/employeeapp/MainActivity.kt
+++ app/src/main/java/com/ajmi/employeeapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.ajmi.employeeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun loginemp(view: View) {
+        var getEname=findViewById<EditText>(R.id.empname)
+        var getEcod=findViewById<EditText>(R.id.empcod)
+        var getDes=findViewById<EditText>(R.id.des)
+        var getCname=findViewById<EditText>(R.id.cmpnm)
+        var getPlace=findViewById<EditText>(R.id.plac)
+        Toast.makeText(this,getEname.text.toString(),Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,getEcod.text.toString(),Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,getDes.text.toString(),Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,getCname.text.toString(),Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,getPlace.text.toString(),Toast.LENGTH_SHORT).show()
     }
 }
